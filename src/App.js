9fd1b856c13b2708bca0660d8267419e9e269954
unknown <chetan.udemy@gmail.com>
2021-06-21 23:00:52 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+function App({ articles }) {
+  const [articlelist, setarticleList] = useState(articles.sort(compare));
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
+  function compare(a, b) {
+    if (a.upvotes < b.upvotes) {
+      return 1;
+    }
+    if (a.upvotes > b.upvotes) {
+      return -1;
+    }
+    return 0;
+  }
 
+  function dateCompare(a, b) {
+    // console.log(new Date(a.date).getTime());
+    if (new Date(a.date).getTime() < new Date(b.date).getTime()) {
+      return 1;
+    }
+    if (new Date(a.date).getTime() > new Date(b.date).getTime()) {
+      return -1;
+    }
+    return 0;
+  }
+
+  const upvotedHandler = () => {
+    const newList = articlelist.sort(compare);
+    setarticleList(newList);
+  };
+
+  const recentHandler = () => {
+    const newList = articlelist.sort(dateCompare);
+    setarticleList(newList);
+  };
+
+  return (
+    <div className='App'>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className='layout-row align-items-center justify-content-center my-20 navigation'>
+        <label className='form-hint mb-0 text-uppercase font-weight-light'>
+          Sort By
+        </label>
+        <button
+          data-testid='most-upvoted-link'
+          className='small'
+          onClick={upvotedHandler}>
+          Most Upvoted
+        </button>
+        <button
+          data-testid='most-recent-link'
+          className='small'
+          onClick={recentHandler}>
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={articlelist} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+function Articles(props) {
+  return (
+    <div className='card w-50 mx-auto'>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.articles.length > 0 &&
+            props.articles.map((item) => (
+              <tr data-testid='article' key='article-index'>
+                <td data-testid='article-title'>{item.title}</td>
+                <td data-testid='article-upvotes'>{item.upvotes}</td>
+                <td data-testid='article-date'>{item.date}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
